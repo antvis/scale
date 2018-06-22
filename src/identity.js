4faad5e962a82fa7fa14e052999257481e6e300a
simaQ <sima.zhang1990@gmail.com>
2018-06-22 23:37:59 +0800

    initial

---
+++ src/identity.js
@@ -0,0 +1,47 @@
+const Base = require('./base');
+const isNumber = require('@antv/util/src/type/isNumber');
+
+class Identity extends Base {
+
+  _initDefaultCfg() {
+    this.isIdentity = true;
+    this.type = 'identity';
+    /**
+     * 输出的值域
+     * @type {Array}
+     */
+    this.range = [ 0, 1 ];
+    /**
+     * 常量值
+     * @type {*}
+     */
+    this.value = null;
+  }
+
+  /**
+   * @override
+   */
+  getText() {
+    return this.value.toString();
+  }
+
+  /**
+   * @override
+   */
+  scale(value) {
+    if (this.value !== value && isNumber(value)) {
+      return value;
+    }
+    return this.range[0];
+  }
+
+  /**
+   * @override
+   */
+  invert() {
+    return this.value;
+  }
+}
+
+Base.Identity = Identity;
+module.exports = Identity;
