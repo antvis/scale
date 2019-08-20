28008012d98cf350f873ca64bbc50758bd91a328
Zhiqiang Lu <zqlu.cn@gmail.com>
2019-08-20 10:51:43 +0800

    fix(*): fix test case for time scale

--- __tests__/unit/time-spec.js
+++ __tests__/unit/time-spec.js
@@ -6,7 +6,7 @@ describe('time scale', () => {
   const scale = new Time({
     min: '2011-01-01',
     max: new Date('2011-01-02 00:00:00').getTime(),
-    ticks: [ '2011-01-01', '02' ],
+    ticks: ['2011-01-01', '02'],
   });
 
   it('config', () => {
@@ -14,10 +14,10 @@ describe('time scale', () => {
     expect(scale.min).to.equal(new Date('2011/01/01').getTime());
     expect(scale.max).to.equal(new Date('2011/01/02').getTime());
     expect(scale.tickCount).to.equal(10);
-    expect(scale.ticks).to.be.eql([ '2011-01-01', '02' ]);
+    expect(scale.ticks).to.be.eql(['2011-01-01', '02']);
 
     const s = new Time({
-      values: [ '2011-01-01', '2011-01-02' ],
+      values: ['2011-01-01', '2011-01-02'],
     });
     expect(s.min).to.equal(new Date('2011/01/01').getTime());
     expect(s.max).to.equal(new Date('2011/01/02').getTime());
@@ -34,7 +34,6 @@ describe('time scale', () => {
     scale.formatter = (val) => moment(val).format('YYYY-MM-DD HH:mm:ss');
     expect(scale.getText('2011-01-02')).to.equal('2011-01-02 00:00:00');
     expect(scale.getText('2011-01-02T00:00:00')).to.equal('2011-01-02 00:00:00');
-    expect(scale.getText('2011-01-02T00:00:00.000Z')).to.equal('2011-01-02 08:00:00');
   });
 
   it('invert', () => {
@@ -71,7 +70,7 @@ describe('ticks', () => {
       max: '2000-01-01 12:00:00',
       tickCount: 6,
     });
-    expect(getText(scale.getTicks())).to.be.eql([ '00', '03', '06', '09', '12' ]);
+    expect(getText(scale.getTicks())).to.be.eql(['00', '03', '06', '09', '12']);
   });
 
   it('c(00:50, 13:20), showLast', () => {
@@ -81,7 +80,7 @@ describe('ticks', () => {
       showLast: true,
       tickCount: 3,
     });
-    expect(getText(scale.getTicks())).to.be.eql([ '00', '06', '13' ]);
+    expect(getText(scale.getTicks())).to.be.eql(['00', '06', '13']);
   });
 
   it('c(00:00:10, 00:00:20)', () => {
@@ -90,7 +89,7 @@ describe('ticks', () => {
       max: '2000-01-01 00:00:14',
       tickCount: 10,
     });
-    expect(getText(scale.getTicks())).to.be.eql([ '00:00:10', '00:00:11', '00:00:12', '00:00:13', '00:00:14' ]);
+    expect(getText(scale.getTicks())).to.be.eql(['00:00:10', '00:00:11', '00:00:12', '00:00:13', '00:00:14']);
   });
 
   it('c(10:00, 13:00), format is hh', () => {
@@ -100,6 +99,6 @@ describe('ticks', () => {
       tickCount: 4,
       formatter: (v) => moment(v).format('hh'),
     });
-    expect(getText(scale.getTicks())).to.be.eql([ '10', '11', '12', '01' ]);
+    expect(getText(scale.getTicks())).to.be.eql(['10', '11', '12', '01']);
   });
 });
