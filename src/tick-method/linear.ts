a3c945d5ef907901ccd6863a3d36195b544fffca
dxq613 <dxq613@gmail.com>
2020-02-04 12:19:00 +0800

    feat(interval): support interval in linear scale

--- src/tick-method/linear.ts
+++ src/tick-method/linear.ts
@@ -1,5 +1,7 @@
 import { ScaleConfig } from '../types';
 import extended from '../util/extended';
+import interval from '../util/interval';
+
 
 /**
  * 计算线性的 ticks，使用 wilkinson extended 方法
@@ -7,6 +9,9 @@ import extended from '../util/extended';
  * @returns 计算后的 ticks
  */
 export default function linear(cfg: ScaleConfig): number[] {
-  const { min, max, tickCount, nice } = cfg;
+  const { min, max, tickCount, nice, tickInterval } = cfg;
+  if (tickInterval) {
+    return interval(min, max, tickInterval).ticks;
+  }
   return extended(min, max, tickCount, nice).ticks;
 }
--- src/tick-method/r-prettry.ts
+++ src/tick-method/r-prettry.ts
@@ -1,12 +1,17 @@
 import { map } from '@antv/util';
 import { ScaleConfig } from '../types';
 import pretty from '../util/pretty';
+import interval from '../util/interval';
+
 /**
  * 计算线性的 ticks，使用 R's pretty 方法
  * @param cfg 度量的配置项
  * @returns 计算后的 ticks
  */
 export default function linearPretty(cfg: ScaleConfig): number[] {
-  const { min, max, tickCount } = cfg;
+  const { min, max, tickCount, tickInterval } = cfg;
+  if (tickInterval) {
+    return interval(min, max, tickInterval).ticks;
+  }
   return pretty(min, max, tickCount).ticks;
 }
