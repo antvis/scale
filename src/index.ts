9f41e1f69f59bd7efe8fe3c75b04dda7faadb649
pearmini <subairui@icloud.com>
2021-03-24 15:38:33 +0800

    refactor: update tick-method export and remove registerTickMethod

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './types';
 
+// scales
 export { Scale } from './scales/base';
 export { Category } from './scales/category';
 export { TimeCat } from './scales/time-cat';
@@ -11,17 +12,14 @@ export { Quantize } from './scales/quantize';
 export { Quantile } from './scales/quantile';
 export { Identity } from './scales/identity';
 
-// 内置的 tick method，以及追加方法
-export {
-  cat,
-  d3Linear,
-  linear,
-  log,
-  pow,
-  quantile,
-  pretty,
-  time,
-  timeCat,
-  timePretty,
-  registerTickMethod,
-} from './tick-methods';
+// tick methods
+export { cat } from './tick-methods/cat';
+export { d3Linear } from './tick-methods/d3-linear';
+export { linear } from './tick-methods/linear';
+export { log } from './tick-methods/log';
+export { pow } from './tick-methods/pow';
+export { quantile } from './tick-methods/quantile';
+export { pretty } from './tick-methods/r-prettry';
+export { time } from './tick-methods/time';
+export { timeCat } from './tick-methods/time-cat';
+export { timePretty } from './tick-methods/time-pretty';
