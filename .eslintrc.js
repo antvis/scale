d985ea70c6b296d0568a4e480f87b3a7462aaaa7
pearmini <subairui@icloud.com>
2021-03-29 19:46:26 +0800

    chore: eslint for import rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,21 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
     semi: 'error',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
+    'import/extensions': 'off',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
