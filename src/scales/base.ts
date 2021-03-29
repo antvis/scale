4a993778833ff2b7f56072105545f9d07ecc32c3
pearmini <subairui@icloud.com>
2021-03-29 20:09:04 +0800

    feat(base): add comments

--- src/scales/base.ts
+++ src/scales/base.ts
@@ -11,27 +11,57 @@ export const DEFAULT_OPTIONS: BaseOptions = {
 };
 
 export default abstract class Base<O extends BaseOptions> {
+  /**
+   * 将定义域里面的一个值，根据转换规则，转换为值域的一个值
+   * @param x 需要转换的值
+   */
   abstract map(x: Primitive): Primitive;
 
+  /**
+   * 将值域里的一个值，据转换规则，逆向转换为定义域里的一个值
+   * @param x 需要转换的值
+   */
   abstract invert(x: Primitive): Primitive;
 
+  /**
+   * 克隆一个新的比例尺，可以用于更新选项
+   */
   abstract clone(): Base<O>;
 
+  /** 比例尺的选项，用于配置数据映射的规则和 ticks 的生成方式 */
   protected options: O = {} as O;
 
+  /**
+   * 构造函数
+   * @param options 需要自定义配置的选项
+   * @param defaultOptions 默认选项，主要用于子类调用 super(options, ownDefaultOptions)
+   */
   constructor(options?: Partial<O>, defaultOptions: O = DEFAULT_OPTIONS as O) {
     assign(this.options, defaultOptions, options);
   }
 
+  /**
+   * 根据 key，获得一个或者全部的选项
+   * @param key 选项的 key
+   * @returns 一个选项或者全部选项
+   */
   public getOptions(key?: string) {
     return key && this.options[key] ? this.options[key] : this.options;
   }
 
+  /**
+   * 更新选项
+   * @param updateOptions 需要更新的选项
+   */
   public update(updateOptions: Partial<O>) {
     assign(this.options, updateOptions);
   }
 
-  public getTicks() {
+  /**
+   * 根据比例尺的配置去生成 ticks，该 ticks 主要用于生成坐标轴
+   * @returns 返回一个 ticks 的数组
+   */
+  public getTicks(): Primitive[] {
     return this.options.tickMethod(this.options);
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,18 @@
 export type Primitive = number | string;
 
 export type BaseOptions<R = Primitive, D = Primitive, T = any> = {
+  /** 当需要映射的值不合法的时候，返回的值 */
   unknown?: string;
+  /** 值域，默认为 [0, 1] */
   range: R[];
+  /** 定义域，默认为 [0, 1] */
   domain: D[];
+  /** tick 格式化函数，会影响数据在坐标轴 axis、legend、tooltip 上的显示 */
   formatter: (x: Primitive) => string;
+  /** tick 个数，默认值为 5 */
   tickCount: number;
+  /** tick 间隔的最大值，默认值为 10 */
   tickInterval: number;
+  /** 计算 ticks 的算法 */
   tickMethod: (options?: T) => Primitive[];
 };
