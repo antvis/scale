76d7f6f49c824209b9724b88f7546f9dd5cc961c
pearmini <subairui@icloud.com>
2021-03-29 18:59:34 +0800

    chore(package.json): replace no-unused-vars of eslint for ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,9 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  rules: {},
+  rules: {
+    semi: 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
 };
