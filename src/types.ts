4fe24ba780ad6915259e62b5ee5cb41ad43fe18c
pearmini <subairui@icloud.com>
2021-03-30 10:46:33 +0800

    feat(types): change the option of base options to optional

--- src/types.ts
+++ src/types.ts
@@ -4,15 +4,15 @@ export type BaseOptions<R = Primitive, D = Primitive, T = any> = {
   /** 当需要映射的值不合法的时候，返回的值 */
   unknown?: string;
   /** 值域，默认为 [0, 1] */
-  range: R[];
+  range?: R[];
   /** 定义域，默认为 [0, 1] */
-  domain: D[];
+  domain?: D[];
   /** tick 格式化函数，会影响数据在坐标轴 axis、legend、tooltip 上的显示 */
-  formatter: (x: Primitive) => string;
+  formatter?: (x: Primitive) => string;
   /** tick 个数，默认值为 5 */
-  tickCount: number;
+  tickCount?: number;
   /** tick 间隔的最大值，默认值为 10 */
-  tickInterval: number;
+  tickInterval?: number;
   /** 计算 ticks 的算法 */
-  tickMethod: (options?: T) => Primitive[];
+  tickMethod?: (options?: T) => Primitive[];
 };
