d863612ad9251e21dc3908dbdb886e51ab9f0388
yuzhanglong <yuzl1123@163.com>
2021-04-07 17:56:27 +0800

    style: beautify code

--- src/scales/band.ts
+++ src/scales/band.ts
@@ -9,7 +9,7 @@ export class Band extends Base<BandOptions> {
 
   // 覆盖默认配置
   constructor(options?: Partial<BandOptions>) {
-    super(options, {
+    const defaultOpts: BandOptions = {
       domain: [],
       range: [0, 1],
       align: 0.5,
@@ -19,7 +19,11 @@ export class Band extends Base<BandOptions> {
       paddingInner: 0,
       paddingOuter: 0,
       padding: 0,
-    });
+      // 默认情况下使用 undefined 而不是 category 默认的配置 -- 自动补充
+      unknown: undefined,
+    };
+
+    super(options, defaultOpts);
 
     this.categoryBase = new Category({
       ...this.options,
@@ -28,33 +32,53 @@ export class Band extends Base<BandOptions> {
     this.adjustBandState();
   }
 
+  /**
+   * 更新/调整 band 配置
+   */
   private adjustBandState() {
     const opt = this.getOptions();
+
+    let rangeStart = opt.range[0];
+    const rangeEnd = opt.range[1];
+
     // 当用户配置了opt.padding 且非 0 时，我们覆盖已经设置的 paddingInner paddingOuter
-    if (opt.padding !== 0) {
+    // 我们约定 padding 的优先级较 paddingInner 和 paddingOuter 高
+    if (opt.padding > 0) {
       opt.paddingInner = opt.padding;
       opt.paddingOuter = opt.padding;
     }
 
-    const n = opt.domain.length;
+    const stepAmount = opt.domain.length;
 
-    let start = opt.range[0];
-    const stop = opt.range[1];
-    let step = (stop - start) / Math.max(1, n - opt.paddingInner + opt.paddingOuter * 2);
+    // range 的计算方式如下：
+    // = stop - start
+    // = (stepAmount * step(n 个 step) )
+    // + (2 * step * paddingOuter(两边的 padding))
+    // - (1 * step * paddingInner(多出的一个 inner))
+    const deltaRange = rangeEnd - rangeStart;
+    const outerTotal = opt.paddingOuter * 2;
+    const innerTotal = stepAmount - opt.paddingInner;
+    opt.step = deltaRange / Math.max(1, outerTotal + innerTotal);
+
+    // 优化成整数
     if (opt.round) {
-      step = Math.floor(step);
+      opt.step = Math.floor(opt.step);
     }
-    start += (stop - start - step * (n - opt.paddingInner)) * opt.align;
 
-    opt.bandWidth = step * (1 - opt.paddingInner);
+    // 基于 align 实现偏移
+    rangeStart += (deltaRange - opt.step * (stepAmount - opt.paddingInner)) * opt.align;
+
+    // 一个 step 的组成如下：
+    // step = bandWidth + step * paddingInner，
+    // 则 bandWidth = step - step * (paddingInner)
+    opt.bandWidth = opt.step * (1 - opt.paddingInner);
+
     if (opt.round) {
-      start = Math.round(start);
+      rangeStart = Math.round(rangeStart);
       opt.bandWidth = Math.round(opt.bandWidth);
     }
 
-    opt.step = step;
-
-    const targetRange = sequence(start, stop, step);
+    const targetRange = sequence(rangeStart, rangeEnd, opt.step);
 
     // 更新 category range
     this.categoryBase.update({
@@ -68,7 +92,9 @@ export class Band extends Base<BandOptions> {
 
   public update(updateOptions: Partial<BandOptions>) {
     super.update(updateOptions);
-    this.categoryBase.update(updateOptions);
+    this.categoryBase.update({
+      ...updateOptions,
+    });
     this.adjustBandState();
   }
 
