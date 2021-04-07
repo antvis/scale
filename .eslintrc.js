a5e64d110d252a5b18c8d4f942de48cc4a6ba235
pearmini <subairui@icloud.com>
2021-04-07 20:42:32 +0800

    chore: eslint use the format rules of prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
