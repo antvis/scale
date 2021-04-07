65fa9b391c0c13d3d937b2f68b470852c7d5518e
pearmini <subairui@icloud.com>
2021-04-07 21:33:37 +0800

    feat(utils): add normalize

---
+++ __tests__/utils/normalize.spec.ts
@@ -0,0 +1,17 @@
+import { createNormalize } from '../../src/utils/normalize';
+
+describe('createNormalize(a, b)', () => {
+  test('createNormalize(a, a) return a constant function of 0.5', () => {
+    const normalize0 = createNormalize(0, 0);
+    const normalize1 = createNormalize(1, 1);
+    expect(normalize0(0)).toBe(0.5);
+    expect(normalize1(1)).toBe(0.5);
+  });
+
+  test('createNormalize(a, b) return a normalize function', () => {
+    const normalize0 = createNormalize(0, 100);
+    expect(normalize0(10)).toBe(0.1);
+    expect(normalize0(50)).toBe(0.5);
+    expect(normalize0(80)).toBe(0.8);
+  });
+});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export { compose } from './compose';
+export { createNormalize } from './normalize';
