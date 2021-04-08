342af65a296ec91de6c51159f58ba47194fe43b7
pearmini <subairui@icloud.com>
2021-04-08 11:55:42 +0800

    feat(utils): add clamp

---
+++ __tests__/utils/clamp.spec.ts
@@ -0,0 +1,15 @@
+import { createClamp } from '../../src/utils';
+
+describe('createClamp', () => {
+  test('createClamp(lo, hi) return a clamp function', () => {
+    const clamp = createClamp(5, 10);
+    expect(clamp(2)).toBe(5);
+    expect(clamp(12)).toBe(10);
+  });
+
+  test('createClamp(hi, lo) exchange hi, lo', () => {
+    const clamp = createClamp(10, 5);
+    expect(clamp(2)).toBe(5);
+    expect(clamp(12)).toBe(10);
+  });
+});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export { compose } from './compose';
 export { createNormalize } from './normalize';
+export { createClamp } from './clamp';
