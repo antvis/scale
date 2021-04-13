5565a878693e41ebf2664beca14d0250822aa466
pearmini <subairui@icloud.com>
2021-04-13 14:26:39 +0800

    feat(threshold): add comments

--- src/scales/threshold.ts
+++ src/scales/threshold.ts
@@ -3,7 +3,11 @@ import { Base } from './base';
 import { Domain, ThresholdOptions, Range } from '../types';
 import { bisect } from '../utils';
 
+/**
+ * 将连续的定义域分段，每一段所有的值对应离散的值域中一个值
+ */
 export class Threshold<O extends ThresholdOptions> extends Base<ThresholdOptions> {
+  /** threshold 的数量 */
   protected n: number;
 
   protected getOverrideDefaultOptions() {
@@ -18,12 +22,18 @@ export class Threshold<O extends ThresholdOptions> extends Base<ThresholdOptions
     this.rescale();
   }
 
+  /**
+   * 二分查找到输入值在哪一段，返回对应的值域中的值
+   */
   public map(x: Domain<ThresholdOptions>) {
     if (!isNumber(x) || Number.isNaN(x)) return this.options.unknown;
     const index = bisect(this.getDomain(), x, 0, this.n);
     return this.options.range[index];
   }
 
+  /**
+   * 在值域中找到对应的值，并返回在定义域中属于哪一段
+   */
   public invert(y: Range<ThresholdOptions>) {
     const { range, domain } = this.options;
     const index = range.indexOf(y);
