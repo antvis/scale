964d3383e7c3ed7cb38af6de4db84431ab1fa1f0
yuzhanglong <yuzl1123@163.com>
2021-04-14 14:38:44 +0800

    test: enhance test

--- __tests__/unit/scales/pow.spec.ts
+++ __tests__/unit/scales/pow.spec.ts
@@ -14,6 +14,22 @@ describe('pow scales', () => {
     expect(scale.map(100)).toStrictEqual(100);
   });
 
+  test('test for poly range or domain', () => {
+    const scale = new Pow({
+      domain: [0, 100, 120],
+      range: [0, 100, 1200],
+      exponent: 2,
+    });
+
+    expect(scale.map(100)).toStrictEqual(100);
+    expect(scale.map(25)).toStrictEqual(6.25);
+    expect(scale.map(110)).toStrictEqual(625);
+
+    expect(scale.invert(100)).toStrictEqual(100);
+    expect(scale.invert(6.25)).toStrictEqual(25);
+    expect(scale.invert(625)).toStrictEqual(110);
+  });
+
   test('invert fn', () => {
     const scale = new Pow({
       domain: [0, 100],
@@ -23,7 +39,7 @@ describe('pow scales', () => {
 
     expect(scale.invert(0)).toStrictEqual(0);
     expect(scale.invert(25)).toStrictEqual(50);
-    // 50.00000000000001
+
     expect(scale.invert(100)).toStrictEqual(100);
     expect(scale.invert(6.25)).toStrictEqual(25);
   });
@@ -70,13 +86,14 @@ describe('pow scales', () => {
     expect(scale.getOptions().domain).toStrictEqual([0, 11]);
   });
 
-  test('test getTicks', () => {
+  test('test clone method', () => {
     const scale = new Pow({
-      domain: [0, 100],
-      range: [0, 100],
-      exponent: 2,
+      domain: [0.6, 10.4],
+      exponent: 10,
+      nice: true,
     });
-    console.log(scale.map(0));
-    console.log(scale.map(50));
+    const newScale = scale.clone();
+    expect(scale.getOptions()).toStrictEqual(newScale.getOptions());
+    expect(scale.getOptions().domain !== newScale.getOptions().domain).toBeTruthy();
   });
 });
