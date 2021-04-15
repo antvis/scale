983ffa30a28da16599291878cc9e3d6b7cc4c2a0
yuzhanglong <yuzl1123@163.com>
2021-04-15 16:07:03 +0800

    chore: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-export { Identity } from './scales/identity';
+export { Band } from './scales/band';
+export { Base } from './scales/base';
+export { Category } from './scales/category';
 export { Constant } from './scales/constant';
+export { Continuous } from './scales/continuous';
+export { Identity } from './scales/identity';
+export { Linear } from './scales/linear';
+export { Point } from './scales/point';
+export { Pow } from './scales/pow';
 export { Threshold } from './scales/threshold';
 
-export type {
+export {
   BandOptions,
   BaseOptions,
   CategoryOptions,
