8071ce7eb81517925d2f818915f2d43c2843b4e5
yuzhanglong <yuzl1123@163.com>
2021-04-15 12:43:12 +0800

    test: add clone method test

--- __tests__/unit/scales/sqrt.spec.ts
+++ __tests__/unit/scales/sqrt.spec.ts
@@ -7,4 +7,13 @@ describe('sqrt scale test', () => {
     // 我们继承了 pow 类，只修改了一个选项，剩余选项的测试应该在父类体现
     expect(exponent).toStrictEqual(0.5);
   });
+
+  test('clone() returns a Sqrt scale with same and independent options', () => {
+    const x1 = new Sqrt();
+    const x2 = x1.clone();
+
+    expect(x2).toBeInstanceOf(Sqrt);
+    expect(x1.getOptions()).toEqual(x2.getOptions());
+    expect(x1.getOptions() !== x2.getOptions()).toBeTruthy();
+  });
 });
