d43ced547b764f4053a69687431a2643b7748a37
pearmini <subairui@icloud.com>
2021-04-16 15:38:34 +0800

    test(tickMethods): update and pass test suite

--- __tests__/unit/tick-methods/pretty.spec.ts
+++ __tests__/unit/tick-methods/pretty.spec.ts
@@ -1,4 +1,4 @@
-import { pretty } from '../../../src/tick-method/pretty';
+import { pretty } from '../../../src/tick-methods/pretty';
 
 describe('pretty ticks', () => {
   it('pretty for c(0, 0)', () => {
--- __tests__/unit/tick-methods/wilkinson-extended.spec.ts
+++ __tests__/unit/tick-methods/wilkinson-extended.spec.ts
@@ -1,47 +1,27 @@
-import { wilkinsonExtended } from '../../../src/tick-method/wilkinson-extended';
+import { wilkinsonExtended } from '../../../src/tick-methods/wilkinson-extended';
 
 describe('wilkinson-extended test', () => {
   test('invalid data for dMin and dMax', () => {
     const res1 = wilkinsonExtended(NaN, NaN);
-    expect(res1).toStrictEqual({
-      min: 0,
-      max: 0,
-      ticks: [],
-    });
+    expect(res1).toStrictEqual([]);
 
     // @ts-ignore
     const res2 = wilkinsonExtended('666666', '66666');
-    expect(res2).toStrictEqual({
-      min: 0,
-      max: 0,
-      ticks: [],
-    });
+    expect(res2).toStrictEqual([]);
   });
 
   test('domain delta is smaller than 1e-15', () => {
     const res1 = wilkinsonExtended(0, 1e-16);
-    expect(res1).toStrictEqual({
-      min: 0,
-      max: 1e-16,
-      ticks: [0],
-    });
+    expect(res1).toStrictEqual([0]);
   });
 
   test('m is to equal to ', () => {
     const res1 = wilkinsonExtended(0, 100, 1);
-    expect(res1).toStrictEqual({
-      min: 0,
-      max: 100,
-      ticks: [0],
-    });
+    expect(res1).toStrictEqual([0]);
   });
 
   test('common usage', () => {
     const res1 = wilkinsonExtended(0, 100, 10);
-    expect(res1).toStrictEqual({
-      max: 100,
-      min: 0,
-      ticks: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-    });
+    expect(res1).toStrictEqual([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]);
   });
 });
