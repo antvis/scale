5ade53a2d0194ac18cdfe39e6dfd059ccedcadf9
pearmini <subairui@icloud.com>
2021-04-20 15:16:33 +0800

    refactor(types): unexport BaseOptions and ContinuousOptions in index

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,8 @@ export { Quantize } from './scales/quantize';
 
 export type {
   BandOptions,
-  BaseOptions,
   OrdinalOptions,
   ConstantOptions,
-  ContinuousOptions,
   IdentityOptions,
   LinearOptions,
   PointOptions,
