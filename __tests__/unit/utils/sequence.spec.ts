dfef3c0d94b1b29855f05765adc55b4b7061847b
yuzhanglong <yuzl1123@163.com>
2021-04-26 00:52:54 +0800

    perf: improve fixed-length array performance

--- __tests__/unit/utils/sequence.spec.ts
+++ __tests__/unit/utils/sequence.spec.ts
@@ -1,15 +1,24 @@
 import { sequence } from '../../../src/utils/sequence';
 
 describe('sequence util test', () => {
-  test('common case', () => {
+  test('Both the first term and the last term are greater than or equal to 0', () => {
     expect(sequence(0, 100, 20)).toStrictEqual([0, 20, 40, 60, 80]);
   });
 
-  test('negative case', () => {
+  test('Both the first term and the last term are less than or equal to 0', () => {
     expect(sequence(-100, 0, 30)).toStrictEqual([-100, -70, -40, -10]);
+    expect(sequence(-100, -10, 20)).toStrictEqual([-100, -80, -60, -40, -20]);
   });
 
-  test('reverse case', () => {
+  test('The last item is less than the first item, but the tolerance is greater than 0, we return an empty array', () => {
     expect(sequence(-100, -200, 50)).toStrictEqual([]);
+  });
+
+  test('Tolerance is equal to 0', () => {
+    expect(sequence(-100, -200, 0)).toStrictEqual([]);
+  });
+
+  test('The last term is equal to the first term', () => {
+    expect(sequence(0, 0, 1000)).toStrictEqual([]);
   });
 });
