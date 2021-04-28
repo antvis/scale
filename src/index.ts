1d573156955b80d57e764da9d43c6098225f7a55
yuzhanglong <yuzl1123@163.com>
2021-04-28 13:13:25 +0800

    chore: expose useful API

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export { Band } from './scales/band';
 export { Ordinal } from './scales/ordinal';
 export { Constant } from './scales/constant';
+export { Continuous } from './scales/continuous';
 export { Identity } from './scales/identity';
 export { Linear } from './scales/linear';
 export { Point } from './scales/point';
@@ -12,6 +13,7 @@ export { Log } from './scales/log';
 export { Quantize } from './scales/quantize';
 export { Quantile } from './scales/quantile';
 export { Time } from './scales/time';
+export { Base } from './scales/base';
 
 // tick-methods
 export { d3Linear } from './tick-methods/d3-linear';
@@ -20,6 +22,7 @@ export { wilkinsonExtended } from './tick-methods/wilkinson-extended';
 
 // scales types
 export type {
+  BaseOptions,
   BandOptions,
   OrdinalOptions,
   ConstantOptions,
@@ -33,6 +36,7 @@ export type {
   SqrtOptions,
   QuantileOptions,
   LogOptions,
+  ContinuousOptions,
 } from './types';
 
 // others
